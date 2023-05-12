688b33a4f8964e4c48fefe7e7b79bac91b3df1b2
root <idrys01@gmail.com>
2023-05-12 00:42:22 -0700

    add function that inserts node into an ordered singly linked list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - insert new node to am ordered singly
+ * linked list
+ * @head: pointer to address of head node
+ * @number: integer value of the new inserted node
+ *
+ * Return: pointer to node or NULL if error occurs
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new, *temp = *head, *prev = NULL;
+
+	new = malloc(sizeof(listint_t));
+	if (head == NULL || new == NULL)
+		return (NULL);
+	new->n = number;
+	for (; temp->next != NULL && temp->n < number;)
+	{
+		prev = temp;
+		temp = temp->next;
+	}
+	if (temp->n > number)
+	{
+		if (prev == NULL)
+			new->next = temp;
+		else
+		{
+			prev->next = new;
+			new->next = temp;
+		}
+	}
+	else
+	{
+		new->next = temp->next;
+		temp->next = new;
+	}
+	return (new);
+}
+
