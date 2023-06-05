0ba2a013fb16056c7a15702e4f16ef7d0f7dd4ca
root <idrys01@gmail.com>
2023-06-05 04:50:01 -0700

    add README file, 0-rectangle and 1-rectangle module

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''This module define Retangle class '''
+
+
+class Rectangle:
+    """ Defines Rectangle class """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+# More OOP in Python
+This project cotains tasks that involves object oriented programming in python
