f5ff8ac84d0b5b8f726dbbfa1cd85d49f5ab7e80
root <idrys01@gmail.com>
2023-06-11 09:57:41 -0700

    add public instance method 'area' to BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This module define a python class called BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Represents a simple Geometry
+    """
+
+    def area(self):
+        """ Calculate the area of geometry
+        """
+
+        raise Exception('area() is not implemented')
