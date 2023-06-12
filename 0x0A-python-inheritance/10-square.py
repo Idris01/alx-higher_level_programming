afd29c4b6afe6721c7582fca2e28762f3ca1b7c3
root <idrys01@gmail.com>
2023-06-12 01:04:25 -0700

    implement square class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Square Module
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a rectangle of size "size"
+
+    Args:
+        size (int): size of the square
+
+    >>> square = Square(5)
+    >>> square.area()
+    25
+    >>> print(square)
+    [Rectangle] 5/5
+    >>> Square('5')
+    Traceback (most recent call last):
+        ...
+    TypeError: width must be an integer
+    >>> Square(-5)
+    Traceback (most recent call last):
+        ...
+    ValueError: width must be greater than 0
+
+    """
+
+    def __init__(self, size):
+        Rectangle.__init__(self, size, size)
+        self.__size = size
