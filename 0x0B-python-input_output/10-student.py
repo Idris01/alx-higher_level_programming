67b0e1f2e3ea73bd8f9b2d37edd9374c50c4a1da
root <idrys01@gmail.com>
2023-06-13 09:20:18 -0700

    update to_json to filter based on attrs argument

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" This module defines student class
+"""
+
+
+class Student:
+    """STudent class
+
+    Args:
+        first_name (str): first name of student
+        last_name (str): last name of student
+        age (int): age of student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """initializes the student object
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrive the dictionary of attrs of student
+        """
+        if (attrs and type(attrs) == list and
+                all([type(attr) == str for attr in attrs])):
+            return {key: self.__dict__[key] for key in attrs if
+                    key in self.__dict__}
+        return self.__dict__
