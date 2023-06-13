18711c72d91af4ea5af77532c77a4517fccd1e38
root <idrys01@gmail.com>
2023-06-13 09:08:36 -0700

    add student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" This module defines student class
+"""
+
+
+class Student:
+    """STudent class
+
+    Args:
+        first_name (str): first name of student
+        last_name (str): last name of student
+        age (int): age of student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """initializes the student object
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrive the dictioary Representation of the student object
+        """
+        return self.__dict__
