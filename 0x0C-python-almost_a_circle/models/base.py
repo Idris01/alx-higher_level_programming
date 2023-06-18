f7bd68c20a133b7aa14fcd3bce88ab2f9f5d9685
root <idrys01@gmail.com>
2023-06-18 01:46:58 -0700

    add safe_to_file static method to base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -45,3 +45,27 @@ class Base:
             return "[]"
 
         return json.dumps(list_dict)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save objects contain in list_objs to file
+
+        Args:
+            list_objs (list): list of objects that are
+            derived from Base
+
+        """
+
+        if list_objs is None or not list_objs:
+            with open('Base.json', 'w', encoding='utf-8') as stream:
+                stream.write("[]")
+                return
+
+        name = f"{type(list_objs[0]).__name__}.json"
+        list_dict = [
+                obj.to_dictionary() for
+                obj in list_objs
+                ]
+
+        with open(name, 'w', encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_dict))
