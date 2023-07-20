4943ad08a5b524f9fba91edbee2854974ea5271c
root <idrys01@gmail.com>
2023-07-20 07:41:04 -0700

    add database and table with id auto_increment

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- creates a database and its table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT  AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256));
