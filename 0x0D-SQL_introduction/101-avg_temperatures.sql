390707cbc87254f1d7291a807b43eab91e66695d
root <idrys01@gmail.com>
2023-07-20 03:24:48 -0700

    add script that prints average temperature

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,4 @@
+-- Prints city and average trmperature ordered
+-- by temperature in descendijg order for the data
+-- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/272/temperatures.sql
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
