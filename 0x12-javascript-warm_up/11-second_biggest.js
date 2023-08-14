377add3d9935586fb4680da23fe589a2a394eaa8
root <idrys01@gmail.com>
2023-08-14 08:07:32 -0700

    remove unecessary complexities

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,17 +6,7 @@ nums = nums.map(item => parseInt(item));
 
 if (nums.length <= 1) console.log(0);
 
-else if (nums.length === 2) console.log(Math.max(...nums));
 else {
   nums = nums.sort((a, b) => a >= b ? -1 : 1);
-  const newList = [];
-  for (const item of nums) {
-    if (newList.length === 2) {
-      console.log(newList[1]);
-      break;
-    }
-    !newList.includes(item) && newList.push(item);
-  }
-  // all numbers in the list are equal
-  if (newList.length === 1) console.log(newList[0]);
+  console.log(nums[1]);
 }
