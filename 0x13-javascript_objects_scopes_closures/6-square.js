cc1d32002c7d1a3b237d50a5eca5116224505399
Idris Adebowale <idrys01@gmail.com>
2023-08-15 13:13:05 +0000

    add checks for size in printChar

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,10 +3,12 @@ const InitialSquare = require('./5-square.js');
 
 class Square extends InitialSquare {
   charPrint (c) {
-    if (!c) {
-      this.print();
-    } else {
-      const line = Buffer.alloc(this.width, c).toString();
+    let myChar = c;
+    if (!myChar) {
+      myChar = 'X';
+    }
+    if (this.width) {
+      const line = Buffer.alloc(this.width, myChar).toString();
       for (let i = 0; i < this.width; i++) {
         console.log(line);
       }
