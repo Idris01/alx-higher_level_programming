201e73e5c4732adcc9296775fd1344999e1cf548
Idris Adebowale <idrys01@gmail.com>
2023-08-31 03:24:52 -0700

    add script that follows redirect and on print response with 200 status code

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Prints the body size of a get request to a url
-curl -sI "$1" | grep content-length | cut -d " " -f 2
+curl -sI "$1" | grep -i content-length | cut -d " " -f 2
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# prints the body of only 200 response code
+curl -sfL "$1"
