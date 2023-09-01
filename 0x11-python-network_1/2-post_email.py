3092b6192f08315309640ccfb2db398df061f8ec
Idris Adebowale <idrys01@gmail.com>
2023-09-01 04:35:28 -0700

    add task 2 file

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module takse a url and an email, then
+send a post request to the url with the email
+"""
+
+__name__ == "__main__":
+    import urllib.request as req
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data
