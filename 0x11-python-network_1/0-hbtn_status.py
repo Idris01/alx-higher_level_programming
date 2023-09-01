542bf41e76349ee0bdd7940ac7483b7f468ca04c
root <idrys01@gmail.com>
2023-09-01 03:59:08 -0700

    add readme file and python script that fetch data from a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module send a get request to a given url
+and print the corresponding data
+"""
+
+if __name__ == "__main__":
+    import urllib.request as req
+
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with req.urlopen(url) as resp:
+        data = resp.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content : {}".format(
+            data.decode('utf-8')))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,2 @@
+# Python Network 
+This project deals with more about python network  using the `urllib` python module
