99db6056345dd1e159468f575c6e2052cf0b73fa
Idris Adebowale <idrys01@gmail.com>
2023-09-26 05:55:00 -0700

    implement writefile with encoding

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-const fs = require('node:fs/promises');
+const fs = require('fs');
 const path = process.argv[2];
 const content = process.argv[3];
 
-(async () => {
-  try {
-    const file = await fs.open(path, 'w');
-    await file.write(content);
-  } catch (error) {
+fs.writeFile(path, content, 'utf-8', (error) => {
+  if (error) {
     console.log(error);
   }
-})();
+});
