506a201cd1bb11088d4d52c10bf0c8782192ef44
Idris Adebowale <idrys01@gmail.com>
2023-09-26 07:43:19 -0700

    update script to use argument

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/people/18/';
+const charUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+const url = process.argv[2];
 
 request(url, (err, res, body) => {
   if (!err) {
-    console.log(JSON.parse(body).films.length);
+    const data = JSON.parse(body).results;
+    console.log(data.filter(item => item.characters.includes(charUrl)).length);
   }
 });
