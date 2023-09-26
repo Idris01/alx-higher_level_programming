1f8eeb0098a08a700f5bd917810e56efef541ce7
Idris Adebowale <idrys01@gmail.com>
2023-09-26 09:06:15 -0700

    make code lean

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,10 +4,4 @@ const fs = require('fs');
 const url = process.argv[2];
 const path = process.argv[3];
 
-request(url, (err, response, body) => {
-  if (!err) {
-    fs.writeFile(path, body, 'utf-8', (error) => {
-      console.log(error);
-    });
-  }
-});
+request(url).pipe(fs.createWriteStream(path, 'utf-8'));
