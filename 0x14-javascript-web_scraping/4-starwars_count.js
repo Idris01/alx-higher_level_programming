0499235e24810faba046aa4c269565b5fd9a7acf
Idris Adebowale <idrys01@gmail.com>
2023-09-28 07:18:55 -0700

    fix search pattern for character

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
-const charUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+const charUrl = 'people/18/';
 const url = process.argv[2];
 
 request(url, (err, res, body) => {
   if (!err) {
     const data = JSON.parse(body).results;
-    console.log(data.filter(item => item.characters.includes(charUrl)).length);
+    const chaData = data.filter(({ characters }) => {
+      for (const cha of characters) {
+        if (cha.endsWith(charUrl)) return true;
+      }
+      return false;
+    });
+    console.log(chaData.length);
   }
 });
